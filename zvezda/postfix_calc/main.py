2b128d873b10cd6fe3fae44c74602248e9ce5aec
tim4ukys <tim4ukys.dev@yandex.ru>
2025-05-18 16:23:03 +0700

    move block code in function main

--- zvezda/postfix_calc/main.py
+++ zvezda/postfix_calc/main.py
@@ -1,22 +1,27 @@
 import postfix_calc
-import sys
 
 print('send exit to go exit')
-while True:
-    s = input('calc: ')
-    if s == 'exit':
-        sys.exit(0)
-    r = None
-    try:
-        r = postfix_calc.postfix_calc(s)
-        print(r)
-    except ZeroDivisionError:
-        print('division by zero.')
-    except ValueError:
-        print('invalid symbol.')
-    except BufferError:
-        print('there are not enough numbers in the expression to calculate.')
-    except RuntimeError:
-        print('there are non-effective numbers in your expression.')
+def main():
+    while True:
+        print('calc: ', end='')
+        expr = ''
+        while True:
+            s = input()
+            if s == 'exit':
+                return
+            try:
+                expr = expr + ' ' + s
+                r = postfix_calc.postfix_calc(expr)
+                print(r)
+            except ZeroDivisionError:
+                print('division by zero.')
+            except ValueError:
+                print('invalid symbol.')
+            except BufferError:
+                print('there are not enough numbers in the expression to calculate.')
+            except RuntimeError:
+                continue
+            break
 
 
+main()
